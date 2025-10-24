090f49f9d2b06777919260152191aa75db448d90
Dilan EESHVARAN <ieeshvarand@gmail.com>
2025-10-24 22:22:13 +0200

    9

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (s, x) {
+  if (x < 0) {
+    return "ERROR";
+  }
+  let res = "";
+  for (let i = 0; i < x; i++) {
+    res += s;
+  }
+  return res;
 };
 
 // Do not edit below this line
