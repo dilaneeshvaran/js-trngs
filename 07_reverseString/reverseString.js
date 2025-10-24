8020a87fc5b9789884c9e6fc888341fd25d4092c
Dilan EESHVARAN <ieeshvarand@gmail.com>
2025-10-24 22:22:20 +0200

    11

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (s) {
+  let len = s.length - 1;
+  let res = "";
+  if (s == "" || s == NaN || len <= 0) return "";
+  for (let i = len; i >= 0; i--) {
+    res += s[i];
+  }
+  return res;
 };
 
 // Do not edit below this line
