3853c4bcad18044676f60909998dbeb65bba748d
Dilan EESHVARAN <ieeshvarand@gmail.com>
2025-10-24 22:22:28 +0200

    13

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, del) {
+  let size = arr.length;
+  let idxDel;
+  for (let i = 0; i < size; i++) {
+    if (arr[i] == del) {
+      idxDel = i;
+    }
+  }
 };
 
 // Do not edit below this line
