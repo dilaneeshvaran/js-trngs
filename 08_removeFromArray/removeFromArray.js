468c52b960af08ccd1abb504095163e7936e6888
Dilan EESHVARAN <ieeshvarand@gmail.com>
2025-11-11 20:42:09 +0100

    8+9
    Signed-off-by: Dilan EESHVARAN <ieeshvarand@gmail.com>

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,11 +1,9 @@
-const removeFromArray = function (arr, del) {
-  let size = arr.length;
-  let idxDel;
-  for (let i = 0; i < size; i++) {
-    if (arr[i] == del) {
-      idxDel = i;
-    }
+const removeFromArray = function (arr, ...del) {
+  if (!Array.isArray(arr) || arr.length === 0 || del == null || del == NaN) {
+    return arr;
   }
+
+  return arr.filter((item) => !del.includes(item));
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (
+    !Number.isInteger(start) ||
+    !Number.isInteger(end) ||
+    end < 0 ||
+    start < 0 ||
+    typeof start !== "number" ||
+    typeof end !== "number"
+  )
+    return "ERROR";
 
+  let res = 0;
+
+  if (start < end) {
+    for (let i = start; i <= end; i++) {
+      res += i;
+    }
+  } else {
+    for (let i = end; i <= start; i++) {
+      res += i;
+    }
+  }
+
+  return res;
 };
 
 // Do not edit below this line
